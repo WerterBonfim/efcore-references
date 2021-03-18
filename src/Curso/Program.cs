a618ad1962c7eac7faca3bafb6eb26d6effa5a16
Werter Bonfim <werter@hotmail.com.br>
2021-03-18 19:07:49 -0300

    chore (insert): insert example

--- src/Curso/Program.cs
+++ src/Curso/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using CursoEFCore.Domain;
+using CursoEFCore.ValueObjects;
+using Microsoft.EntityFrameworkCore;
 
 namespace CursoEFCore
 {
@@ -6,7 +9,29 @@ namespace CursoEFCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InserirDados();
+        }
+
+        private static void InserirDados(){
+            
+            var produto = new Produto {
+                Descricao = "Produto teste",
+                CodigoBarras = "12345512344",
+                Valor = 10m,
+                TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                Ativo = true
+            };
+
+            using var db = new Data.ApplicationContext();
+            db.Produtos.Add(produto);
+            db.Set<Produto>().Add(produto);
+            db.Entry(produto).State = EntityState.Added;
+            db.Add(produto);
+
+            var rowsAffeted = db.SaveChanges();
+
+            Console.WriteLine($"Total Rows {rowsAffeted}");
+
         }
     }
 }
